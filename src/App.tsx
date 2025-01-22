4b0f3d78d142b9492160dca1a40679d50354e4cc
Rajanan <g.rajan1993@gmail.com>
2025-01-22 18:16:02 +0100

    fix:spelling title

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ function App() {
   return (
     <section className='mx-auto container flex items-center justify-center h-screen'>
       <div className="flex items-center flex-col gap-5">
-        <h1 className="text-9xl">JAJAKEN</h1>
+        <h1 className="text-9xl">JAJANKEN</h1>
         <p className="text-3xl">COMING SOON !</p>
       </div>
     </section>
